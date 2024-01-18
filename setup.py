f366131111c8f63251f6747466f1c353d1b6662e
swayambansal01@gmail.com <swayambansal01@gmail.com>
2024-01-18 16:06:36 -0500

    back_to_square1

--- setup.py
+++ setup.py
@@ -9,10 +9,6 @@ clock = pygame.time.Clock()
 # creating the screen 
 screen = pygame.display.set_mode((600,750))
 
-# creating screens now ... 
-test_surface = pygame.Surface((100,200))
-test_surface.fill((0,0,255))
-
 while True:
     # event loop -- 
     # check for all possible event in pygame ...
@@ -22,8 +18,7 @@ while True:
             pygame.quit()               # exit the pygame
             sys.exit()                  # exit all the system processes related to this code 
 
-    screen.fill((175, 215, 70))
-    screen.blit(test_surface, (0,0))
+    screen.fill((175, 215, 70))         # this gives the background a shade of green
 
     # update the screen for everyframe ... 
     pygame.display.update()
