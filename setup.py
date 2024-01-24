f90a0cc43c8261d1345634c625f17ac2677d51bf
swayambansal01@gmail.com <swayambansal01@gmail.com>
2024-01-23 20:18:48 -0500

    created the basic outline for a fruit

--- setup.py
+++ setup.py
@@ -1,25 +1,71 @@
 import pygame
-import sys
+import sys, random
+
+from pygame.math import Vector2
 
 pygame.init()
 
-# cell info 
+# creating a time object clock 
+clock = pygame.time.Clock()
+
+
+# CELL INFO ...  
 # thr cell is a square, where the cell_size is the length of the side of the square
 cell_size = 40
 cell_number = 20
 
+
+# SCREEN INFO ...
 # and since the screen is made up of those cells, we can  have the height and weidth of the screen as
 # height = width = cell_size * cell_numbers
 screen_height = cell_size * cell_number
 screen_width = cell_size * cell_number
 
-# creating a time object clock 
-clock = pygame.time.Clock()
-
 # creating the screen 
-# we have already calculated the screen height and width
-# we will use those to create a screen for the game
+# we have the width and height in the cell file ... 
 screen = pygame.display.set_mode((screen_height, screen_width))
+
+
+# CREATING THE FRUIT ... 
+class FRUIT:
+    # fruit needs to be placed at a position, so we need x and y co-oridnates for it ...
+    # we would also need a rectangle to place the fruit on the screen ...
+    def __init__(self):
+        # the position of the fruit spawn should be random ...
+        self.x = random.randint(0, cell_number - 1)
+        self.y = random.randint(0, cell_number - 1)
+
+        # vectors are more convinient data-structures to use cause of vector operations 
+        # so we will make a position vector (x, y)
+        self.position = Vector2(self.x, self.y) * cell_size
+
+    def draw_fruit(self):
+        # tasks this functions needs to do ...
+            # ** create a rectangle
+            # ** then draw the rectangle
+
+        # ** creating a rectangle 
+        # rectangle takes 4 arguments :
+            # the x, y co-ordinates where you want to place the fruit 
+            # and the size of the rectangle that you want ... 
+
+        # for the x,y -- it would be the same as the x,y of the fruit
+        # and for the size, it would be the same as the cell_size
+            # we had to do a typecasting since vectors give values in float ... 
+            # but rectangle function wants integers 
+        fruit_rectangle = pygame.Rect(int(self.position.x), int(self.position.y), cell_size, cell_size)
+
+        # ** drawing the rectangle 
+        # draw takes three arguments :
+            # the screen you want to display on, which is the 'screen' for us 
+            # the color you want the rectangle to have, which is Red for us
+            # the rectangle you want to draw, for us is fruit_rectangle
+        pygame.draw.rect(screen, (255, 0, 0), fruit_rectangle)
+
+
+
+# creating object of the Fruit ... 
+fruit_obj = FRUIT()
 
 while True:
     # event loop -- 
@@ -31,6 +77,7 @@ while True:
             sys.exit()                  # exit all the system processes related to this code 
 
     screen.fill((175, 215, 70))         # this gives the background a shade of green
+    fruit_obj.draw_fruit()
 
     # update the screen for everyframe ... 
     pygame.display.update()
